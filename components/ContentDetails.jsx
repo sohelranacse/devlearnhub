bef2c3c1b24589475d9ab3b4fe58af6687eb449a
Afiyaayat <37040878+sohelranacse@users.noreply.github.com>
2023-05-02 17:54:31 +0600

    update all

--- components/ContentDetails.jsx
+++ components/ContentDetails.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 import { useTheme } from "next-themes";
 import ShowCode from "./ShowCode";
+import NextContent from "./NextContent";
 
 function ContentDetails({ course }) {
   const { theme } = useTheme();
@@ -71,26 +72,10 @@ function ContentDetails({ course }) {
             {/* post details end */}
 
             {nextTopics && (
-              <div className="py-4 px-2 flex justify-end">
-                <Link
-                  href={`/content/${nextTopics.topics_slug}`}
-                  className="flex text-blue-700 text-xl hover:text-blue-400 hover:text-[20px] hover:italic ease-linear duration-350"
-                >
-                  <svg
-                    className="mt-0.5 mr-2 text-2xl"
-                    stroke="currentColor"
-                    fill="currentColor"
-                    strokeWidth="0"
-                    viewBox="0 0 1024 1024"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-.7 5.2-2L869 536.2a32.07 32.07 0 0 0 0-48.4z"></path>
-                  </svg>
-                  {nextTopics.topics_title}
-                </Link>
-              </div>
+              <NextContent
+                topics_title={nextTopics.topics_title}
+                topics_slug={nextTopics.topics_slug}
+              />
             )}
           </div>
         </div>
---
+++ components/NextContent.jsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+const NextContent = ({ topics_title, topics_slug }) => {
+  return (
+    <div className="flex justify-end py-4 md:py-8">
+      <Link
+        className="flex gap-x-4 md:gap-x-6 items-center w-full px-4 md:px-5 py-6 flex-row-reverse text-right hover:text-blue-400 ease-linear duration-350"
+        href={`/content/${topics_slug}`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          className="duration-100 ease-in transition -rotate-90 text-tertiary dark:text-tertiary-dark inline group-focus:text-link dark:group-focus:text-link-dark"
+        >
+          <g fill="none" fillRule="evenodd" transform="translate(-446 -398)">
+            <path
+              fill="currentColor"
+              fillRule="nonzero"
+              d="M95.8838835,240.366117 C95.3957281,239.877961 94.6042719,239.877961 94.1161165,240.366117 C93.6279612,240.854272 93.6279612,241.645728 94.1161165,242.133883 L98.6161165,246.633883 C99.1042719,247.122039 99.8957281,247.122039 100.383883,246.633883 L104.883883,242.133883 C105.372039,241.645728 105.372039,240.854272 104.883883,240.366117 C104.395728,239.877961 103.604272,239.877961 103.116117,240.366117 L99.5,243.982233 L95.8838835,240.366117 Z"
+              transform="translate(356.5 164.5)"
+            ></path>
+            <polygon points="446 418 466 418 466 398 446 398"></polygon>
+          </g>
+        </svg>
+        <span>
+          <span className="block no-underline text-sm tracking-wide text-secondary dark:text-secondary-dark uppercase font-bold group-focus:text-link dark:group-focus:text-link-dark group-focus:text-opacity-100">
+            Next
+          </span>
+          <span className="block text-lg group-hover:underline">
+            {topics_title}
+          </span>
+        </span>
+      </Link>
+    </div>
+  );
+};
+
+export default NextContent;
